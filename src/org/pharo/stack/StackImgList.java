ef6dfc5e6a82918b9d2d2a8760b4f8d6818caa29
JeanBaptisteArnaud <jbaptiste.arnaud@gmail.com>
2014-04-29 14:51:20 +0200

    prout

---
+++ src/org/pharo/stack/StackImgList.java
@@ -0,0 +1,13 @@
+package org.pharo.stack;
+
+import android.widget.ListView;
+import android.content.Context;
+
+public class StackImgList extends ListView {
+
+	public StackImgList(Context ctx) {
+		super(ctx);
+	}
+
+
+}
